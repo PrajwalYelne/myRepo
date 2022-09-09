9b748616d8347db6d2cec23a06403eb745665156
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-09-09 19:47:57 +0530

    Update ItemType.java

--- ExceptionHandling/iDesign_1/ItemType.java
+++ ExceptionHandling/iDesign_1/ItemType.java
@@ -9,3 +9,20 @@ public class ItemType {
         this.name = name;
         this.deposit = deposit;
         this.costPerDay = costPerDay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(double deposit) {
+        this.deposit = deposit;
+    }

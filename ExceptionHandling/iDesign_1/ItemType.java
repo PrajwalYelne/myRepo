48166c6d2439c404ea27f24b33002128989cea1e
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-09-09 19:47:05 +0530

    Create ItemType.java

---
+++ ExceptionHandling/iDesign_1/ItemType.java
@@ -0,0 +1,11 @@
+package ExceptionHandling.iDesign_1;
+
+public class ItemType {
+    String name;
+    double deposit;
+    double costPerDay;
+
+    public ItemType(String name, double deposit, double costPerDay) {
+        this.name = name;
+        this.deposit = deposit;
+        this.costPerDay = costPerDay;

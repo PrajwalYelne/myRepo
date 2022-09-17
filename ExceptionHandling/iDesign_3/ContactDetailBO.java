865a4a361483ed4ce183e1822271855fd8724f51
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-09-17 13:28:03 +0530

    Update ContactDetailBO.java

--- ExceptionHandling/iDesign_3/ContactDetailBO.java
+++ ExceptionHandling/iDesign_3/ContactDetailBO.java
@@ -1,4 +1,6 @@
 package ExceptionHandling.iDesign_3;
 
 public class ContactDetailBO {
-    static void validate(String 
+    static void validate(String mobile, String alternateMobile) throws DuplicateMobileNumberException {
+        if (mobile.equals(alternateMobile))
+            throw new DuplicateMobileNumberException();

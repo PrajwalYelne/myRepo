7093bc7451581036520aae686f3d239aa48e3fb3
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-10-01 10:48:39 +0530

    i_design_2

--- JDBC/i_design_q2.java
+++ JDBC/i_design_q2.java
@@ -72,3 +72,48 @@ public class TravelClassDAO {
     }
 }
 
+import java.sql.*;
+import java.util.ResourceBundle;
+public class DBConnection {
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {        
+        ResourceBundle rb = ResourceBundle.getBundle("oracle");
+        String url = rb.getString("db.url");
+        String username = rb.getString("db.username");
+        String password = rb.getString("db.password");
+        //fill your code here
+         DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        Connection con = DriverManager.getConnection(url, username, password);
+        
+		return con;
+        
+    }
+}
+import java.io.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         ArrayList<TravelClass> travelclass_list=new ArrayList<TravelClass>();
+        System.out.println("Enter the name of TravelClass :");
+        String name = br.readLine();
+        System.out.println("Enter the description to update :");
+        String description = br.readLine();
+        TravelClassDAO obj=new TravelClassDAO();
+       obj.updateDetail(name,description);
+        travelclass_list=obj.listAllTravelClassess();
+        
+        System.out.println("Updated List of Travel Classes");
+        System.out.format("%-25s %s\n","Name","Description");
+        for(TravelClass travelclass:travelclass_list)
+        {
+         System.out.format("%-25s %s\n",travelclass.getName(),travelclass.getDescription());
+        }
+
+        
+
+        //fill your code here
+    }
+}

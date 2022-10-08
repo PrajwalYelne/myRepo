5165c07fffe47657cb671bb007899473bbeb96c3
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-10-08 11:14:59 +0530

    iDesign_3

---
+++ JDBC/iDesign_3/DBConnection.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+import java.util.ResourceBundle;
+public class DBConnection {
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {        
+       ResourceBundle rb = ResourceBundle.getBundle("oracle");
+        String url = rb.getString("db.url");
+        String username = rb.getString("db.username");
+        String password = rb.getString("db.password");
+        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        Connection con = DriverManager.getConnection(url, username, password);
+        
+		return con;
+    }
+}

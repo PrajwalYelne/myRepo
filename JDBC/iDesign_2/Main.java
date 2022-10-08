05e73a3171b72d21dab5ec72f886911b0acd8116
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-10-08 11:06:03 +0530

    iDesign_2

--- JDBC/iDesign_2/Main.java
+++ JDBC/iDesign_2/Main.java
@@ -1,31 +1,27 @@
-package FundamentalClasses.iDesign_2;
+import java.io.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Enter the number of items:");
-        int n = in.nextInt();
-        in.nextLine();
-
-        System.out.println("Enter the item details in the format(Item Name$Item Type$Item Cost$Item Availability)");
-        Item[] array = new Item[n];
-        for (int i = 0; i < n; i++) {
-            String[] input = in.nextLine().split("[$]");
-            Item item = new Item(input[0], input[1], Integer.parseInt(input[2]), Integer.parseInt(input[3]));
-            array[i] = item;
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         ArrayList<TravelClass> travelclass_list=new ArrayList<TravelClass>();
+        System.out.println("Enter the name of TravelClass :");
+        String name = br.readLine();
+        System.out.println("Enter the description to update :");
+        String description = br.readLine();
+        TravelClassDAO obj=new TravelClassDAO();
+       obj.updateDetail(name,description);
+        travelclass_list=obj.listAllTravelClassess();
+        
+        System.out.println("Updated List of Travel Classes");
+        System.out.format("%-25s %s\n","Name","Description");
+        for(TravelClass travelclass:travelclass_list)
+        {
+         System.out.format("%-25s %s\n",travelclass.getName(),travelclass.getDescription());
         }
 
-        System.out.println("items:");
-        for(int i=0;i<n;i++){
-            Item item = array[i];
-            StringBuilder sb = new StringBuilder(item.toString());
-            if(item.getAvailableQuantity() > 0)
-                System.out.println(sb.append("Available"));
-            else
-                System.out.println(sb.append("Not Available"));
-        }
+ 
     }
 }

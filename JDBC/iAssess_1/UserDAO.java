8dbce8c1911a1ea45ff5fb691aeec54379163d90
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-10-14 21:33:11 +0530

    Create UserDAO.java

---
+++ JDBC/iAssess_1/UserDAO.java
@@ -0,0 +1,28 @@
+import java.text.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.sql.*;
+public class UserDAO{
+
+    public ArrayList<User> listUsers() throws Exception{
+    	ArrayList<User> userList = new ArrayList<User>();
+    	//fill your code here
+      Connection con = DBConnection.getConnection();
+      Statement st = con.createStatement();
+      ResultSet rs = st.executeQuery("select * from \"user\"");
+	//   SimpleDateFormat sdf = new SimpleDateFormat("dd-MON-YYYY");
+      while(rs.next()) {
+      userList.add(new User(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getString(6),rs.getDate(7),rs.getString(8)));
+      }
+      return userList;
+    }
+    public void deleteUser(Integer id) throws Exception{
+    	//fill your code here
+        Connection con = DBConnection.getConnection();
+      Statement st = con.createStatement();
+       int updateCount=st.executeUpdate("delete from  \"user\" where id ="+id);
+
+    }
+    
+}

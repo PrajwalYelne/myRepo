a2a6835498c2fd277b08f2960798f40f605418d3
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-10-14 21:35:45 +0530

    iAssess_1

--- JDBC/iAssess_1/DBConnection.java
+++ JDBC/iAssess_1/DBConnection.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 import java.util.ResourceBundle;
+
 public class DBConnection {
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {        
         ResourceBundle rb = ResourceBundle.getBundle("oracle");

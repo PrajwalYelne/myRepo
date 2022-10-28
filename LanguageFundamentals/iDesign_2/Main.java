85b8894b44b99676b7f050c9c70c8229ef3f4570
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-10-28 09:36:37 +0530

    iDesign_2

--- LanguageFundamentals/iDesign_2/Main.java
+++ LanguageFundamentals/iDesign_2/Main.java
@@ -1,33 +1,16 @@
-package ExceptionHandling.iDesign_2;
-
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter the number of seats to be booked:");
-        int n = in.nextInt();
-        int s = 0;
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		double celsius , Fahrenheit;
+		System.out.println("Temparature in Celsius:");
+		celsius = sc.nextFloat();
 
-        try {
-            if (n > 100 || n < 0)
-                throw new ArrayIndexOutOfBoundsException();
-            int[] array = new int[n];
-            for (int i = 0; i < n; i++) {
-                System.out.println("Enter the seat number " + (i + 1));
-                s = in.nextInt();
-                if (s > 100 || s < 0)
-                    throw new ArrayIndexOutOfBoundsException();
-                else
-                    array[i] = s;
-            }
+		 Fahrenheit =( (celsius*9) / 5 ) + 32;
+		System.out.println("Temparature in Fahrenheit is " + Fahrenheit + "F");
 
-            System.out.println("The seats booked are:");
-            for (int e : array)
-                System.out.println(e);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("java.lang.ArrayIndexOutOfBoundsException: " + (s - 1));
-        }
-    }
+	}
+
 }

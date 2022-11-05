2fe144c2e1a92e71feafdde3c656bc81ed87f117
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-11-05 10:27:20 +0530

    iAssess_2

--- LanguageFundamentals/iAssess_2/Main.java
+++ LanguageFundamentals/iAssess_2/Main.java
@@ -1,41 +1,19 @@
-// package CollectionsAndMap.iAssess_2;
-
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the number of User details to be added");
-        int n = in.nextInt();
-        in.nextLine();
-
-        UserBO list = new UserBO();
-        list.addAll(UserBO.getList());
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter the user " + (i + 1) + " detail in csv format");
-            String[] input = in.nextLine().split(",");
-            User u = new User(input[0], input[1], input[2], input[3]);
-            list.add(u);
-        }
-
-        System.out.println("Name                Contact Number      Username            Email               ");
-
-        for (User u : list) {
-            u.display();
-        }
-
-        System.out.println("Enter the range to be removed from array list");
-        int n1 = in.nextInt();
-        int n2 = in.nextInt();
-
-        list.removeUser(n1, n2);
-
-        System.out.println("Name                Contact Number      Username            Email               ");
-        for (User u : list) {
-            u.display();
-        }
-
-
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the numbers :");
+		int num1 = in.nextInt();
+		int num2 = in.nextInt();
+		int num3 = in.nextInt();
+		if (num1 >= num2 && num1 >= num3)
+        	 System.out.println( num1 + " is the greatest number");
+      	else if (num2 >= num1 && num2 >= num3)
+        	 System.out.println( num2 + " is the greatest number");
+      	else
+        	 System.out.println( num3 + " is the greatest number");
+		// String max_num = new String(Math.max(num1, Math.max(num2, num3)));
+		// System.out.println(max_num + " is the greatest number");
+	}
 }

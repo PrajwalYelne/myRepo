c65f0287017b8c6f8539dd809c1f72954c05d46f
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-11-21 20:49:52 +0530

    Update i_Assess.java

--- Multithreading/i_Assess.java
+++ Multithreading/i_Assess.java
@@ -9,6 +9,7 @@ public class Main extends Thread
     }
     public static void main(String[] args)
     {
+    
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter Number of Counters :");
         int num=sc.nextInt();

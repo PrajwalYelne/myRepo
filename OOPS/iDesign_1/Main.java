970bd36d2fef5c6a8771ed0a749589aaf0040bd7
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-11-24 19:44:13 +0530

    iDesign_1

--- OOPS/iDesign_1/Main.java
+++ OOPS/iDesign_1/Main.java
@@ -1,27 +1,15 @@
-package CollectionsAndMap.iDesign_1;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the number of halls:");
-        int n = in.nextInt();
-        in.nextLine();
-        ArrayList<String> list = new ArrayList();
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter the Hall Name " + (i + 1));
-            list.add(in.nextLine());
-        }
-
-        System.out.println("Enter the hall name to be searched:");
-        String s = in.nextLine();
-
-        if (list.contains(s))
-            System.out.println(s + " hall is found in the list at position " + list.indexOf(s));
-        else
-            System.out.println(s + " hall is not found");
-    }
+	public static void main(String args[]) {
+		//write your code here
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name of the staff:");
+		String name = sc.nextLine();
+		System.out.println("Enter the staff designation:");
+		String designation = sc.nextLine();
+		System.out.println("Enter the department name:");
+		String department = sc.nextLine();
+		Department x = new Department(name,designation,department);
+		x.displayStaff();
+		}
 }

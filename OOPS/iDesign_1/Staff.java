e08762507c7679b3682c63b1a0e4d6fd9605a4dd
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-11-25 19:53:09 +0530

    iDesign_1

---
+++ OOPS/iDesign_1/Staff.java
@@ -0,0 +1,16 @@
+public class Staff {
+	//write your code here
+	private String staffName;
+	private String designation;
+	Staff(String a,String b){
+		staffName = a;
+		designation = b;
+	}
+	public String getStaffName(){
+		return staffName;
+	}
+	public String getDesignation(){
+		return designation;
+	}
+
+}

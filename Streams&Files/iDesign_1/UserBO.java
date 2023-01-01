0f28de14ac013b559514e66f5008183b077fd49a
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-01-01 11:32:41 +0530

    i_Design1

---
+++ Streams&Files/iDesign_1/UserBO.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class UserBO {
+	//Your code here
+	public List<User> readFromFile(BufferedReader br) throws Exception{
+        List<User> a = new ArrayList<User>();
+		
+		String input = br.readLine();
+		while(input != null){
+			String[] data = input.split(",");
+			a.add(new User(data[0],data[1],data[2],data[3]));
+			input = br.readLine();
+		}
+        return a;
+	}
+	public void display(List<User> list){
+		System.out.println("Name            Email                Username        Password");
+		for(User x : list)
+			System.out.println(String.format("%-15s %-20s %-15s %s",
+			x.getName(),x.getEmail(),x.getUsername(),x.getPassword()));
+	}
+}

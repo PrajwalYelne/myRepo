1ce08f69376296865927aa55c4f6f0426dc92008
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-01-01 11:31:38 +0530

    i_Design1

---
+++ Streams&Files/iDesign_1/User.java
@@ -0,0 +1,34 @@
+public class User{
+	//Your code here
+	private String name ,email,username,password;
+	User(String name, String email, String username, String password){
+		this.name = name;
+		this.email = email;
+		this.username = username;
+		this.password = password; 
+	}
+	void setName(String name){
+		this.name = name;
+	}
+	void setPassword(String password){
+		this.password = password;
+	}
+	void setEmail(String email){
+		this.email = email;
+	}
+	void setUsername(String username){
+		this.username = username;
+	}
+	String getName(){
+		return name;
+	}
+	String getPassword(){
+		return password;
+	}
+	String getEmail(){
+		return email;
+	}
+	String getUsername(){
+		return username;
+	}
+}

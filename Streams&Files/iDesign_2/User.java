b0136fde7d2fe3794ccbac863f1cdb128134093a
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-01-11 21:35:09 +0530

    iDesign_2

---
+++ Streams&Files/iDesign_2/User.java
@@ -0,0 +1,34 @@
+public class User {
+	//write your code here
+	private String name ,mobileNumber,username,password;
+	User(String name, String mobileNumber, String username, String password){
+		this.name = name;
+		this.mobileNumber = mobileNumber;
+		this.username = username;
+		this.password = password; 
+	}
+	void setName(String name){
+		this.name = name;
+	}
+	void setPassword(String password){
+		this.password = password;
+	}
+	void setMobileNumber(String mobileNumber){
+		this.mobileNumber= mobileNumber;
+	}
+	void setUsername(String username){
+		this.username = username;
+	}
+	String getName(){
+		return name;
+	}
+	String getPassword(){
+		return password;
+	}
+	String getMobileNumber(){
+		return mobileNumber;
+	}
+	String getUsername(){
+		return username;
+	}
+}

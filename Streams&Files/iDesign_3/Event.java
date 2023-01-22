61ac504db01d5d21e498edc3f2b14db68d925b65
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-01-22 12:21:42 +0530

    iDesign_3

---
+++ Streams&Files/iDesign_3/Event.java
@@ -0,0 +1,17 @@
+public class Event {
+	//fill your code here
+	private String eventName , ownerName;
+	private Integer attendeesCount;
+	Event(){}
+	Event(String eventName, Integer attendeesCount, String ownerName){
+		this.eventName=eventName;
+		this.attendeesCount=attendeesCount;
+		this.ownerName=ownerName;
+	}
+	void setEventName(String eventName){this.eventName=eventName;}
+	String getEventName(){return eventName;}
+	void setOwnerName(String ownerName){this.ownerName=ownerName;}
+	String getOwnerName(){return ownerName;}
+	void setAttendeesCount(Integer attendeesCount){this.attendeesCount=attendeesCount;}
+	Integer getAttendeesCount(){return attendeesCount;}
+}

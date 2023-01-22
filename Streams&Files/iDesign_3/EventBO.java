bbf13838ec5bf85a37e7eea3c57437cf940d6b9f
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-01-22 12:24:52 +0530

    iDesign_3

---
+++ Streams&Files/iDesign_3/EventBO.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.util.*;
+public class EventBO {
+	//fill your code here
+	public List<Event> readFromFile(BufferedReader br) throws Exception{
+		List<Event> a = new ArrayList<Event>();
+		
+		String input = br.readLine();
+		while(input != null){
+			String[] data = input.split(",");
+			if(data.length !=3) break;
+			a.add(new Event(data[0],Integer.parseInt(data[1]),data[2]));
+			input = br.readLine();
+		}
+        return a;
+	}
+	void writeFile(List<Event> eventList,FileWriter fr) throws Exception{
+		BufferedWriter bw = new BufferedWriter(fr);
+		HashSet<String> hs = new HashSet<>();
+		for(Event x : eventList){
+			if(hs.contains(x.getOwnerName())) continue;
+			hs.add(x.getOwnerName());
+			String data = x.getEventName()+","+x.getAttendeesCount()+","+x.getOwnerName();
+			bw.write(data,0,data.length());
+			bw.newLine();
+		}
+		bw.flush();
+	}
+}
